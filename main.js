e8c859755fc062b2034ce5923bec96b8c19c0595
Tornike Kapanadze <tornikek3@gmail.com>
2025-04-24 18:00:31 +0400

    Add form for agent adding

--- main.js
+++ main.js
@@ -24,3 +24,42 @@ dropdownBtns.forEach((button) => {
     }
   });
 });
+
+async function getApartmentInfo() {
+  const token = "9ec0894b-2b23-42c5-85b2-1f039b7280a4";
+
+  const response = await axios.get(
+    "https://api.real-estate-manager.redberryinternship.ge/api/cities",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  let data = await response.data;
+
+  console.log(data);
+}
+
+// getApartmentInfo();
+
+const input = document.getElementById("customFileInput");
+const fileName = document.getElementById("fileName");
+
+input.addEventListener("change", function () {
+  fileName.textContent = this.files[0] ? this.files[0].name : "ფაილი არჩეული არაა";
+});
+
+const agentsEddingBtn = document.querySelector(".adding-agent-button");
+console.log(agentsEddingBtn);
+
+const agentsEddingDiv = document.querySelector(".agents-edding-div");
+
+agentsEddingBtn.addEventListener("click", () => {
+  agentsEddingDiv.classList.remove("hidden");
+});
+
+const resetBtn = document.querySelector(".reset-btn").addEventListener("click", () => {
+  agentsEddingDiv.classList.add("hidden");
+});
